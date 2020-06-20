656b63ae0c89ba24a15741288a4c96311342ef31
HarshithaReddyJ <67042170+HarshithaReddyJ@users.noreply.github.com>
2020-06-20 14:31:37 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,40 @@
+#program to calculate simple intrest 
+P=1000
+R=1
+I=2
+si=P*R*I/100 #simple intrest
+print("simple intrest is",si)
+
+#program to find area of a circle
+r=2
+pi=3.141
+a=pi*r*r#area of a circle
+print("area of a circle is ",a)
+
+#program to find area of a triangle
+b=2
+h=4
+a=1/2*b*h#area of a triangle
+print("area of a triangle is",a)
+
+#program to find area of rectangle
+w=5
+h=3
+a=w*h#area of rectangle
+print("area of rectangle is",a)
+
+#program to find a parameter of a square
+a=2
+p=4*a#parameter of a square
+print("parameter of a square is",p)
+
+#program to find circumference of a circle
+r=3
+pi=3.141
+c=2*pi*r
+print("circumference of a circle is",c)
+
+#program to temprature in celsius to fahraniet
+celsius=37.5
+fahrenheit=(celsius*1.8)+32
+print('%0.if celsius is equal to %0.if degree fahrenheit '%(celsius,fahrenheit))
