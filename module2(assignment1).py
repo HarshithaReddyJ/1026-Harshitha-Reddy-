23d56b6849082b7db9904263d0109287715d38df
HarshithaReddyJ <67042170+HarshithaReddyJ@users.noreply.github.com>
2020-06-26 22:06:29 +0530

    Add files via upload

---
+++ module2(assignment1).py
@@ -0,0 +1,55 @@
+1#Take Two inputs from user & check they are equal or not.
+
+a=int(input("Enter a "))
+b=int(input("Enter b"))
+if  a == b:
+	print("Equal")
+else:
+		print("Not Equal")
+		
+		
+2#Taking 3 numbers from user and check.  1-all are equal ,2 any of two are equal
+
+a=int(input(" enter first number"))
+b=int(input("enter second number"))
+c=int(input("enter third number"))
+if   a == b == c:
+	print("all three numbers are equal")
+else:
+ 	print(" any two are equal")
+ 	
+3#Take two numbers and check whether the sum is greater then 5,less then 5 or equal to 5
+
+a=int(input('enter  a')) 
+b=int(input('enter b '))
+sum = a + b
+print("sum is greater then 5",sum > 5)
+print("sum is equal to 5",sum == 5)
+print("sum is less then 5",sum < 5)
+
+4# passing marks program
+
+pm= 35
+marks=int(input("enrer the scored marks"))
+print("marks is greater then passing marks",a > pm)
+
+5#python prg to find maximum of 3 numbers
+
+a=int(input("enter a"))
+m=a
+a=int(input("enter a"))
+if m > a:
+	m=a
+a=int(input("enter a"))
+if m<a:
+	m=a
+print("maximum among 3 numbers is", m)
+
+
+
+ 	
+
+ 	
+	 	
+ 	
+			
